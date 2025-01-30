4d02b6d643e504d8d2426c052e761955912acb84
MELICI0US <mel4college@gmail.com>
2025-01-29 20:16:54 -0700

    Fix timer

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2752564051"
+    serviceWorkerVersion: "2936699970"
   }
 });
